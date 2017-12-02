0b54d5fd57064335aca0b2e1bcc0208b0a0d02fd
Aidan Markham <apm4337@rit.edu>
2017-12-02 00:55:05 -0500

    Added day night cycle

---
+++ ld40 Unity Project/ld40/Assets/Scripts/DayNight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float time;
+
+    public bool timeProgressionEnabled;
+    public float timeProgressionSpeed;
+
+    public Material starMat;
+    public Color starColor;
+
+    public GameObject sun;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timeProgressionEnabled) {
+            time += timeProgressionSpeed * Time.deltaTime;
+        }
+        if(time > 1)
+        {
+            time = 0;
+        }
+        sun.transform.rotation = Quaternion.Euler(time * 360,0,0);
+
+        starColor.a = -Mathf.Sin(time * (2 *  Mathf.PI));
+
+        starMat.color = starColor;
+	}
+}
