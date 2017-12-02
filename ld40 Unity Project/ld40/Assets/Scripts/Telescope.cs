9060e5be5edb8c8f10a93ba328945dba7df4f42b
Aidan Markham <apm4337@rit.edu>
2017-12-02 14:29:56 -0500

    added bounds

---
+++ ld40 Unity Project/ld40/Assets/Scripts/Telescope.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telescope : MonoBehaviour {
+
+    public Camera fpsCam;
+    public GameObject overlay;
+
+    public float fov;
+    public float zoomFOV;
+
+    public bool hasTelescope;
+    public bool zoomed;
+	// Use this for initialization
+	void Start () {
+        fpsCam.fieldOfView = fov;
+        overlay.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        zoomed = Input.GetKey(KeyCode.LeftShift);
+
+        if(hasTelescope && zoomed)
+        {
+            fpsCam.fieldOfView = zoomFOV;
+            overlay.SetActive(true);
+        }
+        else
+        {
+            fpsCam.fieldOfView = fov;
+            overlay.SetActive(false);
+        }
+        
+	}
+}
